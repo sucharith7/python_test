b9455ccf1e306f74648a96a4d9434ab8899e8f15
sucharith7 <ch.sucharith09@gmail.com>
2020-04-20 21:30:30 +0530

    [sucharith] added factory file

---
+++ MoodAnalyzerFactory.py
@@ -0,0 +1,17 @@
+from MoodAnalyzer1 import MoodAnalyzer1
+class MoodAnalyzerFactory:
+	def createObject(self,message):
+		myObject=MoodAnalyzer1(message)
+		return myObject
+
+	@staticmethod
+	def createMoodAnalyzer(message):
+		moodAnalyzer1=MoodAnalyzer1(message)
+		return moodAnalyzer1
+
+
+
+	@staticmethod
+	def createMoodAnalyzer(message=None):
+		moodAnalyzer1=MoodAnalyzer1(message)
+		return moodAnalyzer1
