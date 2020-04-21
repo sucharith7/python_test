4c2ce784ca74f597c69fdf9e7ee0cc660b6ed326
Sucharith <56544986+sucharith7@users.noreply.github.com>
2020-04-21 12:53:00 +0530

    Update MoodAnalyzerFactory.py

--- MoodAnalyzerFactory.py
+++ MoodAnalyzerFactory.py
@@ -4,13 +4,13 @@ class MoodAnalyzerFactory:
 		myObject=MoodAnalyzer1(message)
 		return myObject
 
+	
 	@staticmethod
 	def createMoodAnalyzer(message):
 		moodAnalyzer1=MoodAnalyzer1(message)
 		return moodAnalyzer1
 
-
-
+	
 	@staticmethod
 	def createMoodAnalyzer(message=None):
 		moodAnalyzer1=MoodAnalyzer1(message)
