593b6473f5eaac8abbdb03e7a5c39391dd5c7106
Sucharith <56544986+sucharith7@users.noreply.github.com>
2020-04-21 14:17:44 +0530

    [sucharith] added exception file

---
+++ MoodAnalyzer.py
@@ -0,0 +1,12 @@
+from CustomException import CustomException
+class MoodAnalyzer:
+	def analyzeMood(self,message):
+		try:
+			if message.__contains__("sad"):
+				return "SAD"
+			else:
+				return "HAPPY"
+		except Exception as exception:
+		 	#return "HAPPY"
+		 	#print("hello")
+			#raise CustomException ("please enter a proper message")
